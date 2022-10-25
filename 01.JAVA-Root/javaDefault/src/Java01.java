c8df4bc3e234c88447b2f67f2206c7a14c1a70a5
Min Jeong-Kim <mindung1127@gmail.com>
2022-10-25 16:16:12 +0900

    첫 시작

---
+++ 01.JAVA-Root/javaDefault/src/Java01.java
@@ -0,0 +1,30 @@
+
+public class Java01 {
+    public static void main(String[] args) {
+        //  자바 클래스는 소스단위 블록
+        //  자바 main 메서드는 클래스의 첫실행 메서드
+        //  자바 한줄 주석
+        /* 여러줄 주석 alt shift a  */
+        
+        // 자바출력1: println() 
+        // - 뒤에 엔터기호가 들어가 있는 출력메서드
+        // 단축키 : syso
+        System.out.println("Hellow Word!");
+        
+        // 자바출력2: print() 
+        // - 뒤에 엔터기호가 없어서 한줄에 나옴
+        System.out.print("Hellow ");
+        System.out.print("Word?! ");
+        System.out.println("너두?");
+
+        // 홑따옴표
+        System.out.println('홑');
+        // 홑따옴표 -> 한글자만 출력됨!
+        // System.out.println('홑따움표');
+        
+        System.out.println(123123);
+        System.out.println(false);
+        System.out.println(0.324234);
+        System.out.println(5+2!=300);
+    }
+}
