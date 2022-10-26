0c20b17ac80bf4a4013fb00a92e5da3d5894a75c
Min Jeong-Kim <mindung1127@gmail.com>
2022-10-26 15:34:23 +0900

    sql

---
+++ 02.SQL-Root/01.DDL/01.CREATE_DB.sql
@@ -0,0 +1,3 @@
+
+CREATE DATABASE `디비명` 
+DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
---
+++ 02.SQL-Root/01.DDL/02.ALTER_DB.sql
@@ -0,0 +1,5 @@
+
+-- [ DB 문자셋 변경하기 ]
+
+ALTER DATABASE `디비명` 
+DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
