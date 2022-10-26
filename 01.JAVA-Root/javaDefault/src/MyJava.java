fb0c4930ecee80d1b004315cb8a1b495f91d3fd0
Min Jeong-Kim <mindung1127@gmail.com>
2022-10-26 14:14:49 +0900

    타입

--- 01.JAVA-Root/javaDefault/src/MyJava.java
+++ 01.JAVA-Root/javaDefault/src/MyJava.java
@@ -6,3 +6,22 @@ public class MyJava{
         
     }
 }   
+
+// 참고: Java는 대소문자를 구분합니다. 
+// "MyClass"와 "myclass"는 의미가 다름
+
+// Java 파일의 이름 은 클래스 이름 과 일치해야함!!! 
+// 파일을 저장할 때 클래스 이름으로 저장
+// 파일 이름 끝에 확장자로 ".java"를 사용함
+
+// public static void main(String[] args){}
+// 클래스에는 반드시 main()가 있어야함. 그래야 실행됨.
+
+// 한 줄 텍스트를 화면에 출력
+// System.out.println() -> 뒤에 줄바꿈 있음!
+// System.out.print() -> 뒤에 줄바꿈 없음
+
+// System.out - Java 내장클래스
+// println() "print line"의 줄임말
+
+// 참고: 중괄호 {}는 코드 블록의 시작과 끝을 표시
